054ba7e03a6a5275ae4e08617c46de0a1d8106cf
Aman <aman.srivastava262@gmail.com>
2020-09-26 13:13:22 +0530

    Removed predefined functions

--- anagram.js
+++ anagram.js
@@ -3,11 +3,27 @@ function check(str1, str2) {
     return false;
   }
 
-  var sortStr1 = str1.split("").sort().join("");
-  var sortStr2 = str2.split("").sort().join("");
+  var sortStr1 = sorts(str1);
+  var sortStr2 = sorts(str2);
 
   return sortStr1 === sortStr2;
 }
+
+function sorts(str) {
+  var arr = str.split('');
+  var tmp;
+  for(var i = 0; i < arr.length; i++){
+    for(var j = i + 1; j < arr.length; j++){
+      if(arr[i] > arr[j]){
+        tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+      }
+    }
+  }
+  return arr.join('');
+}
+
 
 console.log(check("dog", "god"));
 console.log(check("foo", "bar"));
